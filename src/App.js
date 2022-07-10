4a16f15052b270774d0be4464526e5b5e2ce044b
bdong2 <bdong2@naver.com>
2022-07-10 23:10:45 +0900

    create customer.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,49 @@
-import logo from './logo.svg';
+import React,{Component} from 'react';
+import Customer from './components/customer'
 import './App.css';
+
+const customers=[{
+  'id':1,
+  'image':'https://placeimg.com/64/64/1',
+  'name':'방동현 ',
+  'birthday':'880906',
+  'gender':'남자',
+  'job':'회사원'
+},
+{
+'id':2,
+'image':'https://placeimg.com/64/64/2',
+'name':'홍길동 ',
+'birthday':'880906',
+'gender':'남자',
+'job':'회사원'
+},
+{
+  'id':3,
+  'image':'https://placeimg.com/64/64/3',
+  'name':'춘향이 ',
+  'birthday':'880906',
+  'gender':'남자',
+  'job':'회사원'
+  }]
 
 function App() {
   return (
-    <div className="gray-background">
-        <img src={logo} lat="logo"/>
-        <h2>Let's develop management system!</h2>
+    <div>
+    {
+      customers.map(c => {
+        return (
+        <Customer
+        key={c.id}
+        id={c.id}
+        image={c.image}
+        name={c.name}
+        birthday={c.birthday}
+        gender={c.gender}
+        job={c.job}
+      />);
+      })
+    }
     </div>
   );
 }
