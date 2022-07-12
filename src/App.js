cc03617623d0e86903c2323c146b438c2e676d1c
bdong2 <bdong2@naver.com>
2022-07-12 21:46:55 +0900

    apply material ui

--- src/App.js
+++ src/App.js
@@ -2,6 +2,14 @@ import React,{Component} from 'react';
 import Customer from './components/customer'
 import './App.css';
 
+import Table from '@mui/material/Table';
+import TableHead from '@mui/material/TableHead';
+import TableBody from '@mui/material/TableBody';
+import TableRow from '@mui/material/TableRow';
+import TableCell from '@mui/material/TableCell';
+
+
+
 const customers=[{
   'id':1,
   'image':'https://placeimg.com/64/64/1',
@@ -16,7 +24,7 @@ const customers=[{
 'name':'홍길동 ',
 'birthday':'880906',
 'gender':'남자',
-'job':'회사원'
+'job':'디자이너'
 },
 {
   'id':3,
@@ -24,28 +32,31 @@ const customers=[{
   'name':'춘향이 ',
   'birthday':'880906',
   'gender':'남자',
-  'job':'회사원'
+  'job':'프로그래머'
   }]
 
-function App() {
+class App extends Component{
+  render() {
+ 
   return (
     <div>
-    {
-      customers.map(c => {
-        return (
-        <Customer
-        key={c.id}
-        id={c.id}
-        image={c.image}
-        name={c.name}
-        birthday={c.birthday}
-        gender={c.gender}
-        job={c.job}
-      />);
-      })
-    }
+      <Table>
+        <TableHead>
+          <TableRow>
+            <TableCell>번호</TableCell>
+            <TableCell>이미지</TableCell>
+            <TableCell>이름</TableCell>
+            <TableCell>생년월일</TableCell>
+            <TableCell>성별</TableCell>
+            <TableCell>직업</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {customers.map(c => {return (<Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job} />);})}
+        </TableBody>
+      </Table>
     </div>
   );
-}
+}}
 
 export default App;
